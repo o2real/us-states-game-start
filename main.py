01a969a7f05a8327a95b4794fa5e7ce4a418198e
Kingcode <136673119+o2real@users.noreply.github.com>
2024-07-30 17:34:08 +0900

    Guess states

--- main.py
+++ main.py
@@ -14,7 +14,17 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
-    print(answer_state)
+
+    if answer_state == "Exit":
+
+        set1 = set(all_states)
+        set2 = set(guessed_states)
+        left_states = list(set1.symmetric_difference(set2))
+
+        left_data = pandas.DataFrame(left_states)
+        left_data.to_csv("left_states.csv")
+        print(f"You missing {len(left_states)}.")
+        break
 
     if answer_state in all_states:
         t = turtle.Turtle()
@@ -24,5 +34,7 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         guessed_states.append(answer_state)
-
-screen.exitonclick()
+#
+# left_states = all_states - guessed_states
+# df = pandas.DataFrame(left_states)
+# df.to_csv("left_states.csv")
